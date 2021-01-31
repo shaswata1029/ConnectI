6d868e643f3bc7aa4c2abcc3e5540326d55e69fd
shaswata1029 <shaswata1029@gmail.com>
2021-01-31 11:38:26 +0530

    Added view engine

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 8000;
 // Use express router
 app.use('/', require('./routes/index'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err) {
     if (err) {
