e562244a0b220148f05082e48e54d1ea30bf7de4
shaswata1029 <shaswata1029@gmail.com>
2021-01-31 11:32:12 +0530

    Added another controller and route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
-console.log("Router loaded");
+// console.log("Router loaded");
 
 router.get('/', homeController.home);
+
+router.use('/users', require('./users'));
 
 module.exports = router;
