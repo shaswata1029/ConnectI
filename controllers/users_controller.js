0c2761c5c563621603d3c69aab6a18f31dc8ec51
shaswata1029 <shaswata1029@gmail.com>
2021-02-01 15:18:58 +0530

    added more views for sign in and sign up and also used express layouts

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,32 @@
 module.exports.profile = function(req, res) {
 
-    res.end("<h1>User Profile</h1>");
+    // res.end("<h1>User Profile</h1>");
 
+
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
+
+
+}
+
+module.exports.signUp = function(req, res) {
+    return res.render('user_sign_up', {
+        title: "ConnectI | Sign Up"
+    })
+}
+
+// rendered the sign-up page
+
+module.exports.signIn = function(req, res) {
+    return res.render('user_sign_in', {
+        title: "ConnectI | Sign In"
+    })
+}
+
+// rendered the sign-in page
+
+
+module.exports.create = function(req, res) {
 
 }
--- index.js
+++ index.js
@@ -2,10 +2,32 @@ const express = require('express');
 const app = express();
 
 const port = 8000;
+
+const expressLayouts = require('express-ejs-layouts');
+// express layouts for ejs 
+
+
+// database connection
+const db = require('./config/mongoose');
+const User = require('./models/user');
+
+
+// middlewares
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 // Use express router
 app.use('/', require('./routes/index'));
+
+
+// setting up view engines-ejs
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
 
 app.listen(port, function(err) {
     if (err) {
@@ -13,4 +35,4 @@ app.listen(port, function(err) {
         return;
     }
     console.log(`Server is running on port ${port}`);
-})
+});
