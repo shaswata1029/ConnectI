e13eab14974d545e8fa05370f2c28b4dde773c3b
Shaswata Raha <shaswata1029@gmail.com>
2021-04-28 21:49:17 +0530

    passport local strategy

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,68 +1,64 @@
-const passport = require('passport');
+const passport = require("passport");
 
-const LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require("passport-local").Strategy;
 
-const User = require('../models/user');
+const User = require("../models/user");
 
 // authentication using passport and find a user and establish an identity
-passport.use(new LocalStrategy({
-        usernameField: 'email'
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
     },
-    function(email, password, done) {
-        User.findOne({ email: email }, function(err, user) {
-            if (err) {
-                console.log('Error in findind user-->Passport');
-                return done(err);
-            }
-
-            if (!user || user.password != password) {
-                console.log('Invalid username/password');
-                return done(null, false);
-            }
-
-            return done(null, user);
-
-        });
-
-    }
-
-));
-
-// serializing the user to decide which key is to be kept in the cookies
-
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-
-
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
+    function (email, password, done) {
+      User.findOne({ email: email }, function (err, user) {
         if (err) {
-            console.log('Error in findind user-->Passport');
-            return done(err);
+          console.log("Error in findind user-->Passport");
+          return done(err);
+        }
+
+        if (!user || user.password != password) {
+          console.log("Invalid username/password");
+          return done(null, false);
         }
 
         return done(null, user);
+      });
+    }
+  )
+);
 
-    });
+// serializing the user to decide which key is to be kept in the cookies
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
 });
 
-passport.checkAuthentication = function(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    if (err) {
+      console.log("Error in findind user-->Passport");
+      return done(err);
     }
 
-    return res.redirect('/users/sign_in');
+    return done(null, user);
+  });
+});
 
-}
+passport.checkAuthentication = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
 
+  return res.redirect("/users/sign_in");
+};
 
-passport.setAuthenticatedUser = function(req, res, next) {
-    if (req.isAuthenticated()) {
-        res.locals.user = req.user;
-    }
+passport.setAuthenticatedUser = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user;
+  }
 
-    next();
-}
+  next();
+};
 
 module.exports = passport;
